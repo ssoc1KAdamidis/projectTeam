09c3ac35946c1608ee93947173a6cd1ec77078db
ntinab <pixie_11@outlook.com>
2023-01-31 00:19:13 +0200

    - delete ✓
    - edit x
    - dates x
    - image uploader x

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { RouterProvider } from "react-router-dom";
 import router from './router';
 import 'antd/dist/reset.css';
-
+   
 
 const App = () => {
   // return (
